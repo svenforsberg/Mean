c27a51d58c95936551a2aa3eae7bd3b3ffbb708f
Sven Forsberg <sven.johnsson@gmail.com>
2016-10-25 07:50:11 +0200

    Working on increased operating length

--- Mean.cpp
+++ Mean.cpp
@@ -4,25 +4,49 @@
 
 Mean::Mean(int mean_shift)
 {
-	_mean_ct=0;
-	_mean_val=0;
-	_mean_shift=mean_shift;
+	_mean_ct1=0;
+	_mean_ct2=0;
+	_mean_val1=0;
+	_mean_val2=0;
+	_mean_shift1=mean_shift;
+	_mean_shift2=0;
+	if(_mean_shift1>15) _mean_shift2=_mean_shift1-15;
 }
 
 int Mean::CalcMean(int indata,int *utdata)
 {
-  _mean_ct++;
+  
   int result=false;
-  _mean_val+=indata;
-
-  if(_mean_ct==(1u<<_mean_shift))
+  
+  
+  if( _mean_ct1<=(1u<<_mean_shift) || _mean_shift2==0)
   {
-    _mean_val+=1u<<(_mean_shift-1); //Round off
-    *utdata=_mean_val>>_mean_shift;
-    result=true;
-    _mean_val=0;
-    _mean_ct=0;
+	_mean_ct++;
+	_mean_val+=indata;
+	if(_mean_ct==(1u<<_mean_shift))
+	{
+		_mean_val+=1u<<(_mean_shift-1); //Round off
+		*utdata=_mean_val>>_mean_shift;
+		result=true;
+		_mean_val=0;
+		_mean_ct=0;
+	}
   }
+  else
+  {
+	_mean_ct++;
+	_mean_val2+=indata;
+	if(_mean_ct==(1u<<(_mean_shift2))
+	{
+		_mean_val+=1u<<(_mean_shift2-1); //Round off
+		*utdata=_mean_val2>>_mean_shift2;
+		result=true;
+		_mean_val2=0;
+		_mean_ct=0;
+	}
 
+  }
+  
+  
   return result;
 }

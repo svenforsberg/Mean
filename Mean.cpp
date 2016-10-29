208afb891d0f7cd1b54cedd12d73380c38cfd9bf
Sven Forsberg <sven.johnsson@gmail.com>
2016-10-29 13:09:27 +0200

    Almost done

--- Mean.cpp
+++ Mean.cpp
@@ -15,38 +15,40 @@ Mean::Mean(int mean_shift)
 
 int Mean::CalcMean(int indata,int *utdata)
 {
-  
-  int result=false;
-  
-  
-  if( _mean_ct1<=(1u<<_mean_shift) || _mean_shift2==0)
-  {
-	_mean_ct++;
+
+	int result=false;
+	/*
+	Count Mean 1 to full length
+	Add to Mean 2
+	GOTO start
+	*/
+
+	_mean_ct1++;
 	_mean_val+=indata;
-	if(_mean_ct==(1u<<_mean_shift))
+	if(_mean_ct1==(1u<<_mean_shift)) //Mean 1 done.
 	{
-		_mean_val+=1u<<(_mean_shift-1); //Round off
-		*utdata=_mean_val>>_mean_shift;
-		result=true;
-		_mean_val=0;
-		_mean_ct=0;
+		_mean_val1+=1u<<(_mean_shift-1); //Round off
+		_mean_val1=_mean_val1>>_mean_shift;
+		_mean_val2=+_mean_val_1;
+		if(_mean_shift2==0)
+		{
+			result=true;
+			*utdata=_mean_val_1;
+		}
+		_mean_ct2++;
+		_mean_val1=0;
+		_mean_ct1=0;
 	}
-  }
-  else
-  {
-	_mean_ct++;
-	_mean_val2+=indata;
-	if(_mean_ct==(1u<<(_mean_shift2))
+
+	if(_mean_ct2==(1u<<_mean_shift2) && mean_shift2>0)
 	{
-		_mean_val+=1u<<(_mean_shift2-1); //Round off
+		_mean_val2+=1u<<(_mean_shift2-1); //Round off
 		*utdata=_mean_val2>>_mean_shift2;
 		result=true;
 		_mean_val2=0;
-		_mean_ct=0;
+		_mean_ct2=0;
 	}
 
-  }
-  
-  
-  return result;
+
+	return result;
 }
